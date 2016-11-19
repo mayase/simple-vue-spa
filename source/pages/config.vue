f64686bc4cd3abeb805e7ba0f4c91fc4d88f49f5
Максим Яковлев <m.yakovlev@maximatelecom.ru>
2016-11-19 18:06:53 +0300

    netlify test

--- source/pages/config.vue
+++ source/pages/config.vue
@@ -9,7 +9,7 @@
 <template>
     <div class="page-config">
         <div class="page__header">This is config page</div>
-        <p>Your current name is <span class="page__text-highlighted">{{name}}</span>, there you can change it:</p>
+        <p>Your current name is <span class="page__text-highlighted">{{name}}</span>, here you can change it:</p>
 
         <input type="text" v-model="_name" placeholder="your actual name">
         <div class="page__button" @click="changeName(_name)">Save</div>
